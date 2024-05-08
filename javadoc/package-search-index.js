696ea036887abe38438d7d1a95b1c6614dcdc224
Piotr Kuchnowski <kuchnowski.piotr@outlook.com>
2024-05-08 16:41:20 +0200

    Javadoc moved to separated folder

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.edu.pwr.pkuchnowski.FillTheFormAPI"},{"l":"pl.edu.pwr.pkuchnowski.FillTheFormAPI.Controllers"},{"l":"pl.edu.pwr.pkuchnowski.FillTheFormAPI.FormGetters"},{"l":"pl.edu.pwr.pkuchnowski.FillTheFormAPI.Response"},{"l":"pl.edu.pwr.pkuchnowski.FillTheFormAPI.Services"}];updateSearchResults();
